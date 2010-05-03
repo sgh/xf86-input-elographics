94c793a2c3a70f97100b47cdb29d07311532a335
Søren Holm <sh@mikrofyn.com>
2010-05-03 12:04:27 +0200

    Added xorg.conf update functionallity.

--- src/calibrate_xf86elo.cpp
+++ src/calibrate_xf86elo.cpp
@@ -25,6 +25,98 @@ typedef struct _EloShmRec {
   int           report_delay;     /* Delay between touch report packets        */
 }  EloShmRec, *EloShmPtr;
 
+int do_update_xorgconf = 0;
+char xorgconf_file[256] = "/etc/X11/xorg.conf";
+int be_verbose = 0;
+
+void update_xorgconf(int minx, int maxx, int miny, int maxy) {
+	char line[1024];
+	char xorgconf_file_new[256];
+
+	if (be_verbose)
+		fprintf(stderr,"Updating X configration\n");
+	strncpy(xorgconf_file_new, xorgconf_file, sizeof(xorgconf_file_new));
+	strcat(xorgconf_file_new, ".new");
+	FILE* xorgconf = fopen(xorgconf_file, "r+");
+	FILE* newxorgconf = fopen(xorgconf_file_new, "w+");
+
+	if (!xorgconf) {
+		fprintf(stderr,"Unable to open %s\n", xorgconf_file);
+		return;
+	}
+
+	if (!newxorgconf) {
+		fclose(xorgconf);
+		fprintf(stderr,"Unable to open %s\n", xorgconf_file_new);
+		return;
+	}
+
+	while (fgets(line, sizeof(line), xorgconf) != NULL) {
+		const char *minx_match = "#ELOGRAPHICS_MINX";
+		const char *maxx_match = "#ELOGRAPHICS_MAXX";
+		const char *miny_match = "#ELOGRAPHICS_MINY";
+		const char *maxy_match = "#ELOGRAPHICS_MAXY";
+		const char *elomatch  = NULL;
+		const char *optmatch  = NULL;
+		const char *optformat = NULL;
+		int value;
+
+		if (strstr(line, minx_match)) {
+			elomatch  = minx_match;
+			optmatch  = "MINX";
+			optformat = "MinX\" \"%d\" %s\n";
+			value = minx;
+		}
+
+		if (strstr(line, maxx_match)) {
+			elomatch  = maxx_match;
+			optmatch  = "MAXX";
+			optformat = "MaxX\" \"%d\" %s\n";
+			value = maxx;
+		}
+
+		if (strstr(line, miny_match)) {
+			elomatch  = miny_match;
+			optmatch  = "MINY";
+			optformat = "MinY\" \"%d\" %s\n";
+			value = miny;
+		}
+
+		if (strstr(line, maxy_match)) {
+			elomatch  = maxy_match;
+			optmatch  = "MAXY";
+			optformat = "MaxY\" \"%d\" %s\n";
+			value = maxy;
+		}
+
+		if (elomatch) {
+			char tmp[1024];
+			char *ptr;
+			int idx = 0;
+
+			// Uppercase the string in a temporary string
+			while (line[idx]) {
+				tmp[idx] = toupper(line[idx]);
+				idx++;
+			}
+			tmp[idx] = 0;
+
+			// Find the beginning of optionname "MinX/MaxX/MinY/MaxY"
+			ptr = strstr(tmp, optmatch);
+
+			// Write the new option-line
+			if (ptr) {
+				sprintf(line + (ptr-tmp), optformat, value, elomatch);
+			}
+
+		}
+
+		fprintf(newxorgconf,"%s", line);
+	}
+	fclose(xorgconf);
+	fclose(newxorgconf);
+	rename(xorgconf_file_new, xorgconf_file);
+}
 
 class EloCalibrationWidget : public QWidget {
 	int _fd;
@@ -37,19 +129,20 @@ public:
 	EloCalibrationWidget(): _state(0) {
 		_fd = shm_open(SHM_ELOGRAPHICS_NAME, O_RDWR, S_IRWXU);
 		if (_fd<0) {
-			printf("Failed to open shared memory\n");
+			fprintf(stderr,"Failed to open shared memory\n");
 		} else {
 
 			if ( ( priv = (EloShmRec*)mmap(NULL, sizeof(EloShmRec), PROT_READ|PROT_WRITE, MAP_SHARED, _fd, 0)) < 0 ) {
-				printf("Failed to map memory\n");
+				fprintf(stderr,"Failed to map memory\n");
 			}
 		}
 
-		if (_fd > 0) {
-			printf("MinX:%d\n",priv->min_x);
-			printf("MinY:%d\n",priv->min_y);
-			printf("MaxX:%d\n",priv->max_x);
-			printf("MinY:%d\n",priv->max_y);
+		if (_fd > 0 && be_verbose) {
+			fprintf(stderr,"Current calibration :\n");
+			fprintf(stderr,"MinX:%d\n",priv->min_x);
+			fprintf(stderr,"MinY:%d\n",priv->min_y);
+			fprintf(stderr,"MaxX:%d\n",priv->max_x);
+			fprintf(stderr,"MinY:%d\n",priv->max_y);
 		}
 		showFullScreen();
 		repaint();
@@ -88,15 +181,21 @@ public:
 
 		int miny = b_y;
 		int maxy = (float)rect().height()*a_y+b_y;
-		
-		printf("MinX:%d   MaxX:%d\n", minx, maxx );
-		printf("MinY:%d   MaxY:%d\n", miny, maxy );
 
 		priv->min_x = minx;
 		priv->max_x = maxx;
 
 		priv->min_y = maxy;
 		priv->max_y = miny;
+
+		if (be_verbose) {
+			fprintf(stderr,"New calibration :\n");
+			fprintf(stderr,"MinX:%d   MaxX:%d\n", priv->min_x, priv->max_x );
+			fprintf(stderr,"MinY:%d   MaxY:%d\n", priv->min_y, priv->max_y );
+		}
+
+		if (do_update_xorgconf)
+			update_xorgconf(priv->min_x, priv->max_x, priv->min_y, priv->max_y);
 	}
 
 	virtual void resizeEvent(QResizeEvent *) {
@@ -114,7 +213,8 @@ public:
 			case 3: // Bottom right
 				if (_fd > 0) {
 					touch_points[_state] = QPointF(priv->cur_x, priv->cur_y);
-					printf("Raw (%d,%d)\n",priv->cur_x, priv->cur_y);
+					if (be_verbose)
+						fprintf(stderr,"Raw (%d,%d)\n",priv->cur_x, priv->cur_y);
 				}
 				_state++;
 				repaint();
@@ -163,6 +263,32 @@ public:
 };
 
 int main(int argc, char* argv[]) {
+	int opt;
+
+	while ((opt = getopt(argc, argv, "hvxf:")) != -1) {
+		switch (opt) {
+			case 'x':
+				do_update_xorgconf = 1;
+				break;
+			case 'f':
+					strncpy(xorgconf_file, optarg, sizeof(xorgconf_file));
+				break;
+			case 'v':
+				be_verbose = 1;
+				break;
+			case 'h':
+			default: /* '?' */
+					fprintf(stderr, "Usage: %s [-x] [-f xorg.conf]\n",argv[0]);
+					fprintf(stderr, "   -x : Update xorg.conf file\n");
+					fprintf(stderr, "        Option-lines must be formated like this:\n");
+					fprintf(stderr, "        Option \"MinX\" \"value\" #ELOGRAPHICS_MAXX\n");
+					fprintf(stderr, "   -f : Path to configuration file (default: /etc/X11/xorg.conf)\n");
+					fprintf(stderr, "   -v : Be verbose during calibration\n");
+					fprintf(stderr, "   -h : Show this help\n");
+					exit(EXIT_FAILURE);
+		}
+	}
+
 	QApplication app(argc, argv);
 	EloCalibrationWidget ecal;
 	
